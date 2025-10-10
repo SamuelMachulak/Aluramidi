82e7f04b996af25f94193155af405df706c64c0d
SamuelMachulak <machulak.bonfim@escola.pr.gov.br>
2025-10-10 07:47:35 -0300

    onkeydown

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.locaName === 'audio'){
+        elemento.play;
+    }
+    else{
+        console.log('Elementonão encontrado ou seletor inválido');
+    }
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador <listadeTeclas.length; contador ++){
+    const tecla = listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+}
+
+tecla.onclick = function(){
+    tocaSomPom(idAudio);
+}
+
+tecla.onkeydown =function(evento){
+    if (evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+    }
+}
+
 listadeTeclas[0].onclick = tocaSomPom;
